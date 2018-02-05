993bb61524fe991631ce80103c0e6cf0c073075f
Cristian Flores <cristianflores.ee@gmail.com>
2018-02-05 10:36:42 -0300

    copi

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,10 +7,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  parrafo;
+  parrafo = [];
 
   constructor() { 
-    this.parrafo = '<p>Se informa que, para facilitar el ingreso de alumnos nuevos, <strong>el sistema de matr&iacute;cula e inscripci&oacute;n para los alumnos antiguos se cerrar&aacute;</strong> desde el jueves 18 de enero a las 11:30 horas. hasta el domingo 21 de enero a las 20:00 horas. Posteriormente, el sistema se encontrar&aacute;n abierto hasta las fechas indicadas en el informativo anterior.</p>';
+    this.parrafo = [
+      "starting GUESS THE SEED Contest miss your chance to win WAVES Read more on Waves Community Hub",
+      "On January we organized our first Business Meetup of in Ede Netherlands Thanks to everyone who came to the party More meetups in random countries coming",
+      "Whole lotta love goes out to our followers on Twitter Just imagine all those people together tweeting about Waves This definitely says a lot about Waves ecology",
+      "Quick wins on resolving phishing and scam issues in collaboration with Discovered a suspicious page on a social network Report it",
+      "One of the first participants of Waves Lab Simdaq reached hard cap in hours next big star",
+      "Waves Platform is now powering RewardMob a tournament based reward platform for mobile game developers Wanna spoil your players with instant rewards",
+      "",
+      "",
+      "",
+      "",
+      "",
+
+    ]
+    //this.parrafo = '<p>Se informa que, para facilitar el ingreso de alumnos nuevos, <strong>el sistema de matr&iacute;cula e inscripci&oacute;n para los alumnos antiguos se cerrar&aacute;</strong> desde el jueves 18 de enero a las 11:30 horas. hasta el domingo 21 de enero a las 20:00 horas. Posteriormente, el sistema se encontrar&aacute;n abierto hasta las fechas indicadas en el informativo anterior.</p>';
     this.parrafo = this.transformarString(this.parrafo);    
   }
 
